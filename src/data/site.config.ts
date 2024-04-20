577303391e2cb465e5de65355b79fe6adc9d748f
lwu877 <67766822+lwu877@users.noreply.github.com>
2024-04-19 20:46:34 -0400

    Customisation

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -9,9 +9,9 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	author: 'DanielCG', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
+	author: 'lwu877', // Site author
+	title: "Lex Wu's Blog", // Site title.
+	description: 'A blog about the making of the music.', // Description to display in the meta tags
 	lang: 'en-GB',
 	ogLocale: 'en_GB',
 	shareMessage: 'Share this post', // Message to share a post on social media
