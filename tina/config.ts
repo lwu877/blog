2882dac3c564aa79899ad3f849cd62d39019e17b
lwu877 <67766822+lwu877@users.noreply.github.com>
2024-04-20 23:33:36 -0400

    Configured Tina

--- tina/config.ts
+++ tina/config.ts
@@ -16,13 +16,13 @@ export default defineConfig({
   token: process.env.TINA_TOKEN,
 
   build: {
-    outputFolder: "admin",
-    publicFolder: "public",
+    outputFolder: "src/content/",
+    publicFolder: "src/content/posts",
   },
   media: {
     tina: {
-      mediaRoot: "",
-      publicFolder: "public",
+      mediaRoot: "/src/assets/images",
+      publicFolder: "",
     },
   },
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
@@ -40,6 +40,48 @@ export default defineConfig({
             isTitle: true,
             required: true,
           },
+          {
+            type: "datetime",
+            name: "published",
+            label: "Publish Date",
+            required: true,
+          },
+          {
+            type: "boolean",
+            name: "draft",
+            label: "Draft?",
+            required: false,
+          },
+          {
+            type: "string",
+            name: "description",
+            label: "Description",
+            required: false,
+          },
+          {
+            type: "image",
+            name: "image",
+            label: "Hero Image",
+            required: true,
+          },
+          {
+						type: 'string',
+						name: 'tags',
+						required: true,
+						label: 'Tags',
+						description: 'Tags for this post',
+						list: true,
+						ui: {
+							component: 'tags'
+						}
+					},
+          {
+						type: 'string',
+						required: true,
+						name: 'category',
+						label: 'Category',
+						description: 'Select an category for this post',
+					},
           {
             type: "rich-text",
             name: "body",
